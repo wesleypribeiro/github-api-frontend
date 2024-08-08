8927632edd011b35821ac9473d218fcd2516873d
Wesley Paulo Ribeiro <wesley_paulo00@hotmail.com>
2024-08-08 12:19:22 -0300

    teste2

--- src/pages/UsersList.tsx
+++ src/pages/UsersList.tsx
@@ -2,7 +2,7 @@ import { IUser } from "@/interfaces";
 import { useEffect, useState } from "react"
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
-import api from "../api";
+import { api } from "../api";
 
 const UsersList = () => {
   const navigate = useNavigate();
